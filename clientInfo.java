59a0cf8d6d288e62853cba7ea87b4d894f66c328
masoodmawlavizada <mawlav23@gmail.com>
2017-01-04 00:18:17 -0800

    Create clientInfo.java

---
+++ clientInfo.java
@@ -0,0 +1,20 @@
+package com.company;
+
+/**
+ * Created by masoodmawlavizada on 1/3/17.
+ */
+public class clientInfo {
+    String name;
+    int age;
+    int weight;
+    int heightFeet;
+    int heightInches;
+    String gender;
+    String activityLevel;
+    double BMR;
+    double totalCalories;
+    double dailyCarb;
+    double dailyBasal;
+    double carbRatio;
+    double dailyInsulin;
+}
